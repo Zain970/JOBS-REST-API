9a09a2b516528090f989cc74ef46dea151585616
Zain@Zain-ul-Abidin.com <itszain20@gmail.com>
2023-01-22 22:09:30 +0500

    Commit changes

--- app.js
+++ app.js
@@ -42,6 +42,10 @@ app.use(helmet())
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("Jobs api");
+})
+
 // Main Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
